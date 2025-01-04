519eab4a527505a2a0419121710305ac102196c6
flarnrules <breifenberg11@gmail.com>
2025-01-04 00:58:16 -0600

    start tutorial for p5js waveform

---
+++ tutorials/p5js/README.md
@@ -0,0 +1,2 @@
+# waveform tutorial
+https://www.youtube.com/watch?v=uk96O7N1Yo0
---
+++ tutorials/p5js/app/script.js
@@ -0,0 +1,48 @@
+
+var song
+var fft
+
+function preload() {
+    song = loadSound('assets/everglow.mp3')
+
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES)
+    fft = new p5.FFT()
+}
+
+function draw() {
+    background(0)
+    stroke(255)
+    noFill()
+
+    translate(width/2 , height/2 )
+    
+    var wave = fft.waveform()
+
+    for(var t = -1; t <= 1; t += 2){
+        beginShape()
+        for (var i = 0; i <= 180; i++) {
+            var index = floor(map(i, 0, 180, 0, wave.length - 1))
+
+            var r = map(wave[index], -1, 1, 150, 350)
+
+            var x = r * sin(i) * t
+            var y = r * cos(i)
+            vertex(x,y)
+        }
+        endShape()
+    }
+}
+
+function mouseClicked() {
+    if (song.isPlaying()) {
+        song.pause()
+        noLoop()
+    } else {
+        song.play()
+        loop()
+    }
+}
