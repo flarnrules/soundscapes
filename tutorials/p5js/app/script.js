4af26243dd7cbaf25637caeefcef16e4ff06128f
flarnrules <breifenberg11@gmail.com>
2025-01-06 08:55:23 -0600

    add a proto tower image for bg

--- tutorials/p5js/app/script.js
+++ tutorials/p5js/app/script.js
@@ -1,30 +1,54 @@
 
 var song
+var img
 var fft
+var particles = []
+
 
 function preload() {
     song = loadSound('assets/everglow.mp3')
+    img = loadImage('assets/proto_tower_8.png')
 
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES)
+    imageMode(CENTER)
+    rectMode(CENTER)
     fft = new p5.FFT()
+    img.filter(BLUR, 12)
 }
 
 function draw() {
     background(0)
-    stroke(255)
-    noFill()
 
+    // moves to the center
     translate(width/2 , height/2 )
     
+    fft.analyze()
+    amp = fft.getEnergy(20, 200)
+
+    push()
+    if (amp > 230) {
+        rotate(random(-1.5, 1.5))
+    }
+    image(img, 0, 0, width + 100, height + 100)
+    pop()
+
+    var alpha = map (amp, 0, 255, 180, 150)
+    fill (0, alpha)
+    noStroke()
+    rect(0, 0, width, height)
+    stroke(255)
+    strokeWeight(3)
+    noFill()
+
     var wave = fft.waveform()
 
     for(var t = -1; t <= 1; t += 2){
         beginShape()
-        for (var i = 0; i <= 180; i++) {
+        for (var i = 0; i <= 180; i++, i += .5) {
             var index = floor(map(i, 0, 180, 0, wave.length - 1))
 
             var r = map(wave[index], -1, 1, 150, 350)
@@ -35,6 +59,19 @@ function draw() {
         }
         endShape()
     }
+
+    var p = new Particle()
+    particles.push(p)
+
+    for (var i = particles.length -1; i >= 0; i--) {
+    // iterates through array  backwards to prevent flickering effect
+        if (!particles[i].edges()) {
+            particles[i].update(amp > 230)
+            particles[i].show()
+        } else {
+            particles.splice(i, 1)
+        }
+    }
 }
 
 function mouseClicked() {
@@ -44,5 +81,36 @@ function mouseClicked() {
     } else {
         song.play()
         loop()
+    }
+}
+
+class Particle {
+    constructor() {
+        this.pos = p5.Vector.random2D().mult(250)
+        this.vel = createVector(0, 0)
+        this.acc = this.pos.copy().mult(random(0.0001, 0.00001))
+        this.w = random(3, 5)
+        this.color = [random(0,255), random(0,255), random(0,255),]
+    }
+    update(cond) {
+        this.vel.add(this.acc)
+        this.pos.add(this.vel)
+        if (cond) {
+            this.pos.add(this.vel)
+            this.pos.add(this.vel)
+            this.pos.add(this.vel)
+        }
+    }
+    edges() {
+        if (this.pos.x < -width / 2 || this.pos.x > width / 2 || this.pos.y < -height / 2 || this.pos.y > height / 2) {
+            return true
+        } else {
+            return false
+        }
+    }
+    show() {
+        noStroke()
+        fill(this.color)
+        ellipse(this.pos.x, this.pos.y, this.w)
     }
 }
